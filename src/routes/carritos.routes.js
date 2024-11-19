a16538b08c92317b25676d628d7b0e0cbe1c39f5
BenjaminArtunduaga <benjaartunduaga00@gmail.com>
2024-11-19 17:25:42 -0300

    primera entrega

--- src/routes/carritos.routes.js
+++ src/routes/carritos.routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import crypto from "crypto"
+import crypto from "crypto";
+import { __dirname } from "../path.js";
+import { promises as fs } from 'fs';
+import path from "path";
 
 const cartRouter = Router()
 
-const carritos = []
+const carritosPath = path.resolve(__dirname, '../src/db/carritos.json');
+//leer el archivo
+const carritosData = await fs.readFile(carritosPath,'utf-8') //arranca desde la carpeta inicial
+
+const carritos = JSON.parse(carritosData);
+
 //consultar prod guardados en un carrito
 cartRouter.get('/:cid', (req, res) =>{
     const idCarrito = req.params.cid
@@ -17,17 +25,18 @@ cartRouter.get('/:cid', (req, res) =>{
     }
 })
 //crear nuevo carrito
-cartRouter.post('/', (req, res) =>{
+cartRouter.post('/', async (req, res) =>{
     const newCart ={
         id: crypto.randomBytes(5).toString('hex'),
         products : []
 
     }
     carritos.push(newCart)
+    await fs.writeFile(carritosPath, JSON.stringify(carritos))
     res.status(200).send(`carrito creado correctamente con el id ${newCart.id}`)
 })
-
-cartRouter.post('/:cid,products/:pid', (req, res) =>{
+//agregar nueo prod al carrito
+cartRouter.post('/:cid/products/:pid', async (req, res) =>{
     const idCarrito = req.params.cid
     const idProducto = req.params.pid
     const {quantity} = req.body
@@ -35,11 +44,12 @@ cartRouter.post('/:cid,products/:pid', (req, res) =>{
 
     if (carrito) {
     const indice = carrito.products.findIndex(prod => prod.id == idProducto)
-    if(indice != -1){
+    if(indice != -1){  //si existe el prod lo piso con la nueva cantidad
         carrito.products[indice].quantity = quantity
     }else {
         carrito.products.push({id: idProducto, quantity: quantity})
     }
+    await fs.writeFile(carritosPath, JSON.stringify(carritos))
         res.status(200).send("carrito actualiza3 correctamen3")
     } else {
         res.status(404).send({ mensaje: "El carrito no existe" })
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import crypto from "crypto"
+import { __dirname } from "../path.js";
+import { promises as fs } from 'fs';
+import path from "path";
 
 
 const productRouter = Router()
+const productosPath = path.resolve(__dirname, '../src/db/productos.json');
+//leer el archivo
+const productosData = await fs.readFile(productosPath,'utf-8') //arranca desde la carpeta inicial
 
-const productos = []
+const productos = JSON.parse(productosData);
 
 // Consultar productos
 productRouter.get('/', (req, res) => {
@@ -26,7 +32,7 @@ productRouter.get('/:pid', (req, res) => {
 })
 
 //Crear un nuevo producto
-productRouter.post('/', (req, res) => {
+productRouter.post('/', async (req, res) => {
     const { title, description, code, price, category, stock } = req.body
     const nuevoProducto = {
         id: crypto.randomBytes(10).toString('hex'), //Me genera un id unico
@@ -40,11 +46,12 @@ productRouter.post('/', (req, res) => {
         thumbnails: []
     }
     productos.push(nuevoProducto)
+    await fs.writeFile(productosPath, JSON.stringify(productos))
     res.status(201).send({ mensaje: `Producto creado correctamente con el id: ${nuevoProducto.id}` })
 })
 
 //Actualiza un producto dado su id y pido los datos a actualizar del cuerpo de la peticion
-productRouter.put('/:pid', (req, res) => {
+productRouter.put('/:pid', async (req, res) => {
     const idProducto = req.params.pid
     const { title, description, code, price, category, stock, thumbnails, status} = req.body
     const indice = productos.findIndex(prod => prod.id == idProducto)
@@ -59,6 +66,7 @@ productRouter.put('/:pid', (req, res) => {
         productos[indice].status = status 
         productos[indice].category = category 
         productos[indice].thumbnails = thumbnails 
+        await fs.writeFile(productosPath, JSON.stringify(productos))
 
         res.status(200).send({ mensaje: "Producto actualizado" })
     } else {
@@ -67,11 +75,12 @@ productRouter.put('/:pid', (req, res) => {
 })
 
 //Elimina un producto dado su id
-productRouter.delete('/:pid', (req, res) => {
+productRouter.delete('/:pid', async (req, res) => {
     const idProducto = req.params.pid
     const indice = productos.findIndex(prod => prod.id == idProducto)
     if (indice != -1) {
         productos.splice(indice, 1)
+        await fs.writeFile(productosPath, JSON.stringify(productos))
         res.status(200).send({ mensaje: 'Producto eliminado' })
     } else {
         res.status(404).send({ mensaje: "El producto no existe" })
