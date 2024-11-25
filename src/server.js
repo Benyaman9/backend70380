f95c4d546da2f84bd4f1e68b579c00fdaa6684bf
BenjaminArtunduaga <benjaartunduaga00@gmail.com>
2024-11-25 20:09:06 -0300

    handle

--- src/server.js
+++ src/server.js
@@ -1,16 +1,35 @@
 import express from 'express'
-import productRouter from './routes/productos.routes.js'
+import { create } from 'express-handlebars'
+import path from 'path'
 import { __dirname } from './path.js'
+import productRouter from './routes/productos.routes.js'
+
 import cartRouter from './routes/carritos.routes.js'
 import multerRouter from './routes/imagenes.routes.js'
 
 
 const app = express()
+const hbs = create()
 const PORT = 8080
-// estos express.json y url encoded son middlewares de aplicacion por que son globales
+
+//Middlewares de aplicacion
 app.use(express.json()) //Para manejar JSON en las peticiones
 app.use(express.urlencoded({extended: true}))
-app.use('/static', express.static(__dirname + '/public')) // defino carpeta publica como destino de archivos estaticos
+//Configuracion de hbs para localizacion de plantillas y extension de archivo
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+//Establezco el directorio de las vistas
+
+
+
+
+app.use('/static', express.static(__dirname + '/public'))//Defino la carpeta publica como destino de los archivos estaticos
+app.set('views', path.join(__dirname, '/views'))
+console.log(__dirname);
+
+//app.set('views',__dirname + '/src/views')
+
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter)
 
